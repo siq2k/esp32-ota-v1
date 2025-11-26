af30f92caa388de12619fb75003aa6ed8dfe7147
naz_uddin <naz.uddin@informa.com>
2025-11-26 13:22:42 +0000

    Lnie error

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import ssd1306
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
 i2c.scan()
-i2c.readfrom(0x3a, 4)   # read 4 bytes from device with address 0x3a
+
 i2c.writeto(0x3a, '12') # write '12' to device with address 0x3a
 
 buf = bytearray(10)     # create a buffer with 10 bytes
