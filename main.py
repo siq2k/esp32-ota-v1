b4bb457d6377c9c5dd1a857f9bd57d73285648e0
naz_uddin <naz.uddin@informa.com>
2025-11-26 12:42:19 +0000

    Changed IMport

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from ota import OTAUpdater
 
 import ssd1306
 
-i2c = machine.SoftI2C(scl=machine.Pin(22),sda=machine.Pin(21))
+i2c = machine.I2C(scl=machine.Pin(22),sda=machine.Pin(21))
 oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
 oled.text('Hello-World_1',0,0)
-oled.text('Hello-World_2',0,10)
-oled.text('Hello-World_3',0,20)
 
 oled.show()
 
