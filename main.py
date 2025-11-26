3479a02e683e486156a4f5ec535421db0dbc6d20
naz_uddin <naz.uddin@informa.com>
2025-11-26 13:24:07 +0000

    again

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ import ssd1306
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
 i2c.scan()
-
 i2c.writeto(0x3a, '12') # write '12' to device with address 0x3a
-
-buf = bytearray(10)     # create a buffer with 10 bytes
-i2c.writeto(0x3a, buf)  # write the given buffer to the peripheral
 
 led = Pin(12, Pin.OUT)
 
