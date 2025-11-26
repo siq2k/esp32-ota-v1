c210102c0fa6733ba70c9ac1eb0b021de2ffda08
naz <n124m.haque@gmail.com>
2025-11-26 01:26:12 +0000

    Longer Loop

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ota import OTAUpdater
 led = machine.Pin(12, machine.Pin.OUT)
 
 while True:
-    for i in range(20):
+    for i in range(100):
         led.on()
         time.sleep(0.5)
         led.off()
