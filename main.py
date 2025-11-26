6e5c27c63e3e85f14ea3b75a8760c004ec738946
naz_uddin <naz.uddin@informa.com>
2025-11-26 12:33:12 +0000

    Oled Added

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import machine
 import time
 from ota import OTAUpdater
 
+import ssd1306
+
+i2c = machine.SoftI2C(scl=machine.Pin(22),sda=machine.Pin(21))
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
+oled.text('Hello-World_1',0,0)
+oled.text('Hello-World_2',0,10)
+oled.text('Hello-World_3',0,20)
+
+oled.show()
+
 led = machine.Pin(12, machine.Pin.OUT)
 
 while True:
