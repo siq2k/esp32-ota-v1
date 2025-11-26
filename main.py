121f979f00ae29d239277f6b0e3e40c158680dda
naz <n124m.haque@gmail.com>
2025-11-26 00:44:27 +0000

    Added blink

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# This is a test file
-# Just been updated
-print("Hello World")
-print("New Update")
-print("Modified")
-print("New Added")
+import machine
+import time
+
+led = machine.Pin(12, machine.Pin.OUT)
+
+for i in range(10):
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
