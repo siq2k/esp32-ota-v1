eb071cb94a7033782f3f1b75a3c251dbebd93b7f
naz <n124m.haque@gmail.com>
2025-11-26 01:15:16 +0000

    Round About

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import machine
 import time
+from ota import OTAUpdater
 
 led = machine.Pin(12, machine.Pin.OUT)
 
-for i in range(5):
+while True:
     led.on()
-    time.sleep(1)
+    time.sleep(0.5)
     led.off()
-    time.sleep(1)
+    time.sleep(0.5)
+    ota_updater.download_and_install_update_if_available()
