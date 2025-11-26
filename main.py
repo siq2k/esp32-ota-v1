3172fa8eaecbef6aef5baa8a423089f01851692a
naz_uddin <naz.uddin@informa.com>
2025-11-26 12:53:27 +0000

    Show

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ i2c = I2C(scl=Pin(22),sda=Pin(21))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 display.poweroff()
 display.poweron()
-display.text('Hello-World_1', 0, 0, 1)
+display.text('Hello, World!', 0, 0, 1)
 display.show()
 
 led = Pin(12, Pin.OUT)
